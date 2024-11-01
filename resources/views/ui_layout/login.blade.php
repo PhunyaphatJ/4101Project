7f181a8bd60e1a4a2a88f5fdfbbfb82f8b07919a
BoonyaritL <zoldyck15123@gmail.com>
2024-11-01 18:41:50 +0700

    edit  hover login

--- resources/views/ui_layout/login.blade.php
+++ resources/views/ui_layout/login.blade.php
@@ -2,13 +2,25 @@
 @section('title', 'Login')
 @section('login', 'select_menu_color')
 @section('body_header', 'Login')
+@section('style')
+    <style>
+        /* hover button */
+        button[type="submit"]:hover {
+            background-color: #ccc;
+            color: #333;
+            border-color: #333;
+            cursor: pointer;
+        }
+    </style>
+@endsection
 @section('body_content')
     <div class="row justify-content-center">
         <div class="col-5 col-item">
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="form-floating my-4">
-                    <input type="email" class="form-control rounded-5 ps-4" name="email" placeholder="0000000000@rumail.ru.ac.th">
+                    <input type="email" class="form-control rounded-5 ps-4" name="email"
+                        placeholder="0000000000@rumail.ru.ac.th">
                     <label class="ps-4" for="email">Email address</label>
                 </div>
                 @error('email')
@@ -25,8 +37,8 @@
                         {{ $message }}
                     </div>
                 @enderror
-                @if($invalid)
-                <p id="invalid-text" class="text-danger text-center">Email หรือรหัสผ่านไม่ถูกต้อง</p>
+                @if ($invalid)
+                    <p id="invalid-text" class="text-danger text-center">Email หรือรหัสผ่านไม่ถูกต้อง</p>
                 @endif
                 <button class="btn submit_color w-100 py-3 my-4 rounded-5" type="submit">Login</button>
             </form>
